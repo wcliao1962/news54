5488ca37ffcb81f133fab56294ee7b7e1bb1e354
廖文忠 <wcliao1962@gmail.com>
2017-09-25 00:08:00 +0800

    加入QR code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,6 +81,10 @@
                 <div class="title m-b-md">
                     Laravel News54
                 </div>
+                <div class="visible-print text-center">
+                    {!! QrCode::size(100)->generate(Request::url()); !!}
+                    <p>Scan me to return to the original page.</p>
+                </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
